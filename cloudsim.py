74631244b98c1f80b2ea8cda4d0a65c7c5abfabb
Lalith Suresh <suresh.lalith@gmail.com>
2010-10-14 09:22:14 +0100

    Segregate scheduling algorithms from main file

--- cloudsim.py
+++ cloudsim.py
@@ -2,6 +2,7 @@ import sys
 from SimPy.Simulation import *
 from SimPy.SimPlot import *
 import random
+from scheduling_algos import *
 
 def change_total_tasks(scenario, value):
     scenario.total_tasks += value
@@ -154,57 +155,6 @@ class TaskGenerator(Process):
           
 
 currentMachine=0
-
-# SCHEDULING ALGORITHMS DEFINITION BEGIN #
-
-def weighted_random_schedule(machineList, scenario):
-    total = 0
-    for m in machineList:
-        total += m.factor
-    value = random.uniform(0, total)
-    total = 0
-    for m in machineList:
-        total += m.factor
-        if value < total:
-            return m
-    return machineList[len(machineList)-1]
-
-def random_schedule(machineList, scenario):
-    return machineList[random.randint(0,len(machineList)-1)]
-
-def least_full(machineList, scenario):
-    lower=-1
-    lowerMachines=[]
-
-    for machine in machineList:
-        queue_size=machine.get_queue_size()
-        if(queue_size < lower or lower == -1):
-            lower=queue_size
-            lowerMachine=[machine]
-        elif (queue_size == lower):
-            lowerMachine.append(machine)
-
-    if(len(lowerMachine) > 0):
-        return lowerMachine[random.randint(0, len(lowerMachine)-1)]
-    
-    return machineList[0]
-
-def round_robin(machineList, scenario):
-    global currentMachine
-    machineToReturn=machineList[currentMachine]
-    currentMachine=(currentMachine+1)%len(machineList)
-    return machineToReturn
-
-def fill_queue(machineList, scenario):
-    global currentMachine
-    if(not machineList[currentMachine].can_enqueue_an_element()):
-        nextMachine=(currentMachine+1)%len(machineList)
-        while nextMachine != currentMachine and not machineList[nextMachine].can_enqueue_an_element():
-           nextMachine=(nextMachine+1)%len(machineList)
-        currentMachine=nextMachine
-    return machineList[currentMachine]
-
-# SCHEDULING ALGORITHMS DEFINITION END #
 
 #CONSTANTS
 SCHEDULER_MAX_QUEUE_SIZE=5000000
---
+++ scheduling_algos.py
@@ -0,0 +1,48 @@
+from SimPy.Simulation import *
+
+def weighted_random_schedule(machineList, scenario):
+    total = 0
+    for m in machineList:
+        total += m.factor
+    value = random.uniform(0, total)
+    total = 0
+    for m in machineList:
+        total += m.factor
+        if value < total:
+            return m
+    return machineList[len(machineList)-1]
+
+def random_schedule(machineList, scenario):
+    return machineList[random.randint(0,len(machineList)-1)]
+
+def least_full(machineList, scenario):
+    lower=-1
+    lowerMachines=[]
+
+    for machine in machineList:
+        queue_size=machine.get_queue_size()
+        if(queue_size < lower or lower == -1):
+            lower=queue_size
+            lowerMachine=[machine]
+        elif (queue_size == lower):
+            lowerMachine.append(machine)
+    
+    if(len(lowerMachine) > 0):
+        return lowerMachine[random.randint(0, len(lowerMachine)-1)]
+            
+    return machineList[0]
+
+def round_robin(machineList, scenario):
+    global currentMachine
+    machineToReturn=machineList[currentMachine]
+    currentMachine=(currentMachine+1)%len(machineList)
+    return machineToReturn
+
+def fill_queue(machineList, scenario):
+    global currentMachine
+    if(not machineList[currentMachine].can_enqueue_an_element()):
+        nextMachine=(currentMachine+1)%len(machineList)
+        while nextMachine != currentMachine and not machineList[nextMachine].can_enqueue_an_element():
+           nextMachine=(nextMachine+1)%len(machineList)
+        currentMachine=nextMachine
+    return machineList[currentMachine]
