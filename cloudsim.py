2714150be8eaeea41cadc3bb41fdf6a2ec76ef1d
Lalith Suresh <suresh.lalith@gmail.com>
2010-10-14 09:53:20 +0100

    Segregated GridMachine from cloudsim.py

--- cloudsim.py
+++ cloudsim.py
@@ -9,13 +9,8 @@ from SchedulingAlgos import *
 from TaskGenerator import *
 from Task import *
 from AbstractResource import *
+from GridMachine import *
 
-class GridMachine(AbstractResource):
-    def __init__(self, number, queue_max_size=10, factor=1):
-        AbstractResource.__init__(self, capacity=1, name="M"+str(number)+':'+str(factor),
-                            queue_max=queue_max_size)
-        self.factor = factor
-    
 currentMachine=0
 
 #CONSTANTS
