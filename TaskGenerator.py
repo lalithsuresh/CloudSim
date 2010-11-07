4e0ac3301f0b8bfda2ca1fb699c436c7561e310b
Lalith Suresh <suresh.lalith@gmail.com>
2010-11-07 13:50:41 +0000

    Web request mode enabled. TaskGenerator reworked for future extensions.

--- TaskGenerator.py
+++ TaskGenerator.py
@@ -35,8 +35,20 @@ class TaskGenerator(Process):
             highMemBound = each
 
             if (mode == 'W'):
-                print "Not developed yet"
-                exit (-1)
+                # Note that job-task correspondence is one-to-one
+                webTaskBucket = []
+
+                for taskId in xrange (startingTaskId, startingTaskId + numJobs):
+                    name = "Job%s-%s" % (0, taskId)
+                    reqInstr = int (self.cpuRandomObject.uniform (lowInstrBound, highInstrBound))
+                    reqMem = int (self.memRandomObject.uniform (lowMemBound, highMemBound))
+                    # JobId = 0, TaskId = taskId
+                    jobInTask = Job (name, reqInstr, reqMem, taskId, 0, self.scenario)
+                    task = Task ("Task" + str(taskId), taskId, [jobInTask], self.scenario)
+                    webTaskBucket.append(task)
+                
+                self.tasklist.append (webTaskBucket)
+
             elif (mode == 'S'):
                 # Create a set of jobs for the task
                 joblist = []
@@ -48,17 +60,42 @@ class TaskGenerator(Process):
                     joblist.append (Job (name, reqInstr, reqMem, startingTaskId, jobId, self.scenario))
              
                 task = Task ("Task" + str(startingTaskId), startingTaskId, joblist, self.scenario)
-                self.tasklist.append (task)
+                self.tasklist.append ([task])
+
+    def getNJobsFromBucket (self, i, rateOfJobGeneration, N, jobsToBeAdded):
+        '''
+        Gets N jobs from the current Task Bucket.
+        1) If there are at least rateOfJobGeneration number of jobs within the current
+           task, then return a list of that many jobs.
+        2) Else, extract as many jobs as possible from the current task, and extract the
+           remainder number of jobs from the next task (recursion used).
+        '''
+        if N == 0:
+            return []
+        try:
+            for k in xrange (rateOfJobGeneration):
+                jobsToBeAdded.append (self.tasklist[i][0].joblist.pop(0))
+        except IndexError: # Not enough jobs in the current task
+            try:
+                self.tasklist[i].pop(0) # Move to next task
+                jobsToBeAdded + self.getNJobsFromBucket(i, rateOfJobGeneration, rateOfJobGeneration - len(jobsToBeAdded), jobsToBeAdded)
+            except IndexError: # Exhausted all tasks in the bucket. Move on.
+                return jobsToBeAdded
+
+        return jobsToBeAdded
 
     def run(self, finish):
 
         # Populate the task list
         self.generate_tasks()
-       
+         
         activate (self.scenario.scheduler, self.scenario.scheduler.run())
 
-        times = len(self.tasklist)
-        for i in xrange(times):
+        times = len (self.tasklist)
+
+        # For each task bucket
+        for i in xrange(len(self.task_parameters)):
+
             mode,\
             startingTaskId,\
             numJobs,\
@@ -69,19 +106,17 @@ class TaskGenerator(Process):
             lowMemBound,\
             highMemBound = self.task_parameters[i]
 
-            while (numJobs):
-                jobsToBeAdded = []
-                try:
-                    for i in xrange (rateOfJobGeneration):
-                        jobsToBeAdded.append (self.tasklist[0].joblist.pop(0))
-                except IndexError:
-                    self.tasklist.pop(0)
-                    for i in xrange (len(jobsToBeAdded) - rateOfJobGeneration):
-                        jobsToBeAdded.append (self.tasklist[0].joblist.pop(0))
+            times = len(self.tasklist[i])
+            # For each task in a task bucket
+            for x in xrange(times):
 
-                for each in jobsToBeAdded:
-                    self.scenario.scheduler.addJob(each)
-                    numJobs -= 1
+                while (numJobs):
 
-                yield hold, self, 1
-                #print "Current time: " + str(now())
+                    jobsToBeAdded = self.getNJobsFromBucket (i, rateOfJobGeneration, rateOfJobGeneration, [])
+
+                    for each in jobsToBeAdded:
+                        self.scenario.scheduler.addJob(each)
+                        numJobs -= 1
+
+                    yield hold, self, 1
+                    print "Current time: " + str(now())
